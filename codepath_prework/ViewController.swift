d2848cd0de319102cabb99be31f58c4c51d32ca6
Adan Lopez <adan1001917@gmail.com>
2022-02-04 19:20:35 -0800

    Added Split fucntionality for up to 3 people

--- codepath_prework/ViewController.swift
+++ codepath_prework/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +27,21 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        
-        totalLabel.text = String(format: "$%.2f", total)
+        tipAmountLabel.text = String(format: "$%.2f", tip / Double(splitControl.selectedSegmentIndex + 1))
+        totalLabel.text = String(format: "$%.2f", total / Double(splitControl.selectedSegmentIndex + 1))
     }
     
+    @IBAction func calculateSplit(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip / Double(splitControl.selectedSegmentIndex + 1))
+        totalLabel.text = String(format: "$%.2f", total / Double(splitControl.selectedSegmentIndex + 1))
+    }
 }
 
